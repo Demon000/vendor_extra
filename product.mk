d6d256fd6c5868d5b97e0c98f55d3567d07f9d71
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-27 21:34:29 +0200

    extra: add ih8sn integration
    Change-Id: I58e5097bf9558940289bc548f0c836b0b1531561

--- product.mk
+++ product.mk
@@ -1,3 +1,15 @@
 # Repainter integration
 PRODUCT_PACKAGES += \
     RepainterServicePriv \
+
+# I hate Safety Net
+PRODUCT_PACKAGES += \
+    ih8sn
+
+DEVICE := $(subst lineage_,,$(TARGET_PRODUCT))
+IH8SN_CONFIG_PATH := $(LOCAL_PATH)/prebuilt/etc/ih8sn_$(DEVICE).conf
+
+ifneq ($(wildcard $(IH8SN_CONFIG_PATH)),)
+PRODUCT_COPY_FILES += \
+    $(IH8SN_CONFIG_PATH):/system/etc/ih8sn.conf
+endif
